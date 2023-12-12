679551d2f7475532d9133ac1c0d543a15e444318
Dawid Janta Lipiński <djantalipinski@gmail.com>
2023-12-12 14:32:06 +0100

    SharedModule implemented

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -12,7 +11,7 @@ import { RegisterComponent } from './components/register/register.component';
     RegisterComponent
   ],
   imports: [
-    CommonModule,
+    SharedModule,
     AuthRoutingModule
   ],
   exports: [
--- src/app/modules/clients/clients.module.ts
+++ src/app/modules/clients/clients.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { ClientsRoutingModule } from './clients-routing.module';
 import { ClientsComponent } from './clients.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -10,7 +9,7 @@ import { ClientsComponent } from './clients.component';
     ClientsComponent
   ],
   imports: [
-    CommonModule,
+    SharedModule,
     ClientsRoutingModule
   ], 
   exports: [
