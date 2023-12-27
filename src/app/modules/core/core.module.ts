d5b61e4da790124c4ce80ae22cda43efa69b9c5d
Dawid Janta Lipiński <djantalipinski@gmail.com>
2023-12-27 16:10:11 +0100

    loadind spinner implemented using http interceptor

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -4,16 +4,21 @@ import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { SpinnerInterceptor } from './interceptors/spinner.interceptor';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, SpinnerComponent],
   imports: [
     SharedModule,
     RouterModule,
     BrowserAnimationsModule,
     HttpClientModule,
   ],
-  exports: [HeaderComponent],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true },
+  ],
+  exports: [HeaderComponent, SpinnerComponent],
 })
 export class CoreModule {}
